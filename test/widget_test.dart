e5ba6a46158f77327ec1bf019e94f555e8a30a41
Sankar Prasad Biswas <sankarbiswas07@gmail.com>
2023-11-19 20:42:52 +0530

    TestCase: [Functionality]: Basic Calculator

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,57 @@
 import 'package:basics/app/app.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  final inputX = find.byKey(const Key("InputX"));
+  final inputY = find.byKey(const Key("InputY"));
+  final resultZ = find.byKey(const Key("ResultZ"));
+  final divideButton = find.byIcon(CupertinoIcons.divide);
+  final multiplyButton = find.byIcon(CupertinoIcons.multiply);
+  final addButton = find.byIcon(CupertinoIcons.add);
+  final minusButton = find.byIcon(CupertinoIcons.minus);
+  final refreshButton = find.byIcon(CupertinoIcons.refresh_thin);
+
   testWidgets("[UI Component]: Basic Calculator", (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
+    expect(inputX, findsOneWidget);
+    expect(inputY, findsOneWidget);
+    expect(resultZ, findsOneWidget);
+    expect(divideButton, findsOneWidget);
+    expect(multiplyButton, findsOneWidget);
+    expect(addButton, findsOneWidget);
+    expect(minusButton, findsOneWidget);
+    expect(refreshButton, findsOneWidget);
+  });
 
-    expect(find.byKey(const Key("InputX")), findsOneWidget);
-    expect(find.byKey(const Key("InputY")), findsOneWidget);
-    expect(find.byKey(const Key("ResultZ")), findsOneWidget);
-    expect(find.byIcon(CupertinoIcons.divide), findsOneWidget);
-    expect(find.byIcon(CupertinoIcons.multiply), findsOneWidget);
-    expect(find.byIcon(CupertinoIcons.add), findsOneWidget);
-    expect(find.byIcon(CupertinoIcons.minus), findsOneWidget);
-    expect(find.byIcon(CupertinoIcons.refresh_thin), findsOneWidget);
+  testWidgets("[Functionality]: Basic Calculator", (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    await tester.enterText(inputX, "40");
+    await tester.enterText(inputY, "20");
+
+    // Divide : 40 / 20
+    await tester.tap(divideButton);
+    await tester.pump();
+    // Check if ResultZ has the expected text value
+    var resultZWidget = tester.widget<Text>(resultZ);
+    expect(resultZWidget.data, "2.0000");
+
+    // Multiply : 40 * 20
+    await tester.tap(multiplyButton);
+    await tester.pump();
+    resultZWidget = tester.widget<Text>(resultZ);
+    expect(resultZWidget.data, "800.0000");
+
+    // add : 40 + 20
+    await tester.tap(addButton);
+    await tester.pump();
+    resultZWidget = tester.widget<Text>(resultZ);
+    expect(resultZWidget.data, "60.0000");
+
+    // Sub : 40 - 20
+    await tester.tap(minusButton);
+    await tester.pump();
+    resultZWidget = tester.widget<Text>(resultZ);
+    expect(resultZWidget.data, "20.0000");
   });
 }
